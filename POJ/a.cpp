8f5edd4aa585bbaba96b84167037a6458a815c49
gblym <mylbg4evol@gmail.com>
2015-10-03 16:28:06 +0800

    POJ

---
+++ POJ/a.cpp
@@ -0,0 +1,13 @@
+/*************************************************************************
+    > File Name: a.cpp
+    > Author: gblym
+    > Mail: mylbg4evol@gmail.com 
+    > Created Time: Sat Oct  3 16:26:52 2015
+ ************************************************************************/
+
+#include<iostream>
+using namespace std;
+int main()
+{
+	return 0;
+}
